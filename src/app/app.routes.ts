faf1ce77968e9e316b2284bfd9432f97a08a9d15
Lázaro <lazaroyaesta@gmail.com>
2024-12-09 22:18:28 +0100

    feat: Add project structure & routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './core/layout/layout.component';
 
-export const routes: Routes = [];
+const AppRoutes = {
+  HOME: {
+    PATH: 'home',
+    IMPORT: '@home/home.routes',
+  },
+};
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: AppRoutes.HOME.PATH,
+        loadChildren: () =>
+          import(AppRoutes.HOME.IMPORT).then((m) => m.HOME_ROUTES),
+      },
+    ],
+  },
+  {
+    path: '**',
+    redirectTo: AppRoutes.HOME.PATH,
+  },
+];
