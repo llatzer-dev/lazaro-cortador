6cbd7c79d01b16a0166e0316a75a2ecee5f069f0
Lázaro <lazaroyaesta@gmail.com>
2024-12-10 23:51:48 +0100

    feat: add theme colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#631111",
+        secondary: "#2c5b02",
+        "secondary-hover": "#618442",
+        "primary-black": "#000",
+        "secondary-black": "#222",
+        "tertiary-black": "#323232",
+        "primary-white": "#fff",
+        "secondary-white": "#a1a1aa",
+      },
+    },
   },
   plugins: [],
 };
