417896adab185852f114da1e4fd0b0cf33870b01
Lázaro <lazaroyaesta@gmail.com>
2024-12-15 16:13:06 +0100

    feat: router internal link id

--- src/app/routes/home/components/hero/hero.component.ts
+++ src/app/routes/home/components/hero/hero.component.ts
@@ -1,8 +1,9 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-hero',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
