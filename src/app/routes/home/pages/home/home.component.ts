4e78198acf199459f2db7ecd5440bd52feadc404
Lázaro <lazaroyaesta@gmail.com>
2025-06-13 22:48:35 +0200

    feat: add FAQ component

--- src/app/routes/home/pages/home/home.component.ts
+++ src/app/routes/home/pages/home/home.component.ts
@@ -3,10 +3,16 @@ import { HeroComponent } from '../../components/hero/hero.component';
 import { ColeccionesComponent } from '../../components/colecciones/colecciones.component';
 import { ServiciosComponent } from '../../components/servicios/servicios.component';
 import { AutoDestroyService } from '@app/core/services/utils/auto-destroy.service';
+import { FaqComponent } from '../../components/faq/faq.component';
 
 @Component({
   selector: 'app-home',
-  imports: [HeroComponent, ColeccionesComponent, ServiciosComponent],
+  imports: [
+    HeroComponent,
+    ColeccionesComponent,
+    ServiciosComponent,
+    FaqComponent,
+  ],
   providers: [AutoDestroyService],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
