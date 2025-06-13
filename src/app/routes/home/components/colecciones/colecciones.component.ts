a9dabbc1b1370b7b5c2ba6835c62ed503acb77f8
Lázaro <lazaroyaesta@gmail.com>
2025-06-13 14:56:52 +0200

    feat: masonry with photo gallery

--- src/app/routes/home/components/colecciones/colecciones.component.ts
+++ src/app/routes/home/components/colecciones/colecciones.component.ts
@@ -1,9 +1,24 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import {
+  AfterContentInit,
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  Component,
+  ElementRef,
+  inject,
+  NgZone,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { ColeccionService } from '../../services/coleccion.service';
 import { Coleccion } from '@app/core/models/coleccion.model';
-import { Observable, of, takeUntil, tap } from 'rxjs';
+import { Observable, of, takeUntil } from 'rxjs';
 import { AsyncPipe, NgOptimizedImage } from '@angular/common';
 import { AutoDestroyService } from '@app/core/services/utils/auto-destroy.service';
+import PhotoSwipeLightbox from 'photoswipe/lightbox';
+import PhotoSwipe from 'photoswipe';
+import { CheckPlatformUtility } from '@app/core/services/utils/check-platform.utility';
+// @ts-ignore
+import Masonry from 'masonry-layout';
 
 @Component({
   selector: 'app-colecciones',
@@ -12,13 +27,49 @@ import { AutoDestroyService } from '@app/core/services/utils/auto-destroy.servic
   styleUrl: './colecciones.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ColeccionesComponent implements OnInit {
+export class ColeccionesComponent
+  implements OnInit, AfterContentInit, AfterViewInit
+{
   colecciones$: Observable<Coleccion[]> = of([]);
+  private checkPlatform = inject(CheckPlatformUtility);
+
+  // @ViewChild('masonryGrid', { static: false }) masonryGrid!: ElementRef;
+  @ViewChild('masonryGallery', { static: false }) masonryGallery!: ElementRef;
 
   constructor(
     private readonly coleccionService: ColeccionService,
-    private readonly destroy$: AutoDestroyService
+    private readonly destroy$: AutoDestroyService,
+    private ngZone: NgZone
   ) {}
+
+  ngAfterContentInit(): void {
+    if (this.checkPlatform.checkIfBrowser()) {
+      this.ngZone.runOutsideAngular(() => {
+        const lightbox = new PhotoSwipeLightbox({
+          gallery: '#gallery--getting-started',
+          children: 'a',
+          pswpModule: PhotoSwipe,
+        });
+
+        lightbox.init();
+      });
+    }
+  }
+
+  ngAfterViewInit(): void {
+    if (this.checkPlatform.checkIfBrowser()) {
+      this.ngZone.runOutsideAngular(async () => {
+        // @ts-ignore
+        const Masonry = (await import('masonry-layout')).default;
+
+        new Masonry(this.masonryGallery.nativeElement, {
+          itemSelector: '.grid-item',
+          columnWidth: 200,
+          gutter: 20,
+        });
+      });
+    }
+  }
 
   ngOnInit(): void {
     this.colecciones$ = this.coleccionService
